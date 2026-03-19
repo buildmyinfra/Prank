f4f259237f9337efcfb95d6451008375dbd5e5b8
buildmyinfra <rohit.sahani@buildmyinfra.com>
2026-03-19 13:26:57 +0000

    new

--- Prank.py
+++ Prank.py
@@ -1,1 +1,11 @@
 
+import pyautogui as rudraa
+import time
+
+message = "Hello, this is automated message!"
+time.sleep(5)  # give you time to click on the chat/input box
+
+for i in range(10):
+    rudraa.write(message)
+    rudraa.press("enter")
+    time.sleep(0.5)
